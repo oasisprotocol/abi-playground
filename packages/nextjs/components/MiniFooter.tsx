9fa58c3a6d2c84e63b09fef224fbafc7c952f3ed
lukaw3d <lukajeran@oasislabs.com>
2024-06-01 04:35:18 +0200

    Update footer

--- packages/nextjs/components/MiniFooter.tsx
+++ packages/nextjs/components/MiniFooter.tsx
@@ -1,28 +1,25 @@
-import { HeartIcon } from "@heroicons/react/24/outline";
-import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
-
 export const MiniFooter = () => {
   return (
     <div className="flex justify-center items-center gap-1 text-xs w-full mt-10 z-10">
       <div className="mb-1">
-        <a href="https://github.com/BuidlGuidl/abi.ninja" target="_blank" rel="noreferrer" className="link">
-          Fork me
-        </a>
-      </div>
-      <span>·</span>
-      <div className="flex justify-center items-center gap-2">
-        <p className="m-0 text-center">
-          Built with <HeartIcon className="inline-block h-4 w-4" /> at
-        </p>
-        <a
-          className="flex justify-center items-center gap-1"
-          href="https://buidlguidl.com/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <BuidlGuidlLogo className="w-3 h-5 pb-1" />
-          <span className="link">BuidlGuidl</span>
-        </a>
+        <span>
+          Source at:
+          <a
+            href="https://github.com/lukaw3d/abi-playground-sapphire"
+            target="_blank"
+            rel="noreferrer"
+            className="link"
+          >
+            lukaw3d/abi-playground-sapphire
+          </a>
+        </span>
+        &nbsp;·&nbsp;
+        <span>
+          Forked from:
+          <a href="https://github.com/BuidlGuidl/abi.ninja" target="_blank" rel="noreferrer" className="link">
+            BuidlGuidl/abi.ninja
+          </a>
+        </span>
       </div>
     </div>
   );
