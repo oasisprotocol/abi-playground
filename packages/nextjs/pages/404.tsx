073eab055a810f4cc659af7f4d3ca2a29101419a
lukaw3d <lukajeran@oasislabs.com>
2024-06-01 19:44:40 +0200

    Revert "rafgraph/spa-github-pages"
    This reverts commit 7e6048c9706300014910cc1990b3ed0c16a98204.

--- packages/nextjs/pages/404.tsx
+++ packages/nextjs/pages/404.tsx
@@ -1,12 +1,1 @@
-if (typeof window !== "undefined") {
-  // Idea from https://github.com/rafgraph/spa-github-pages
-  // See index page
-  const baseUrl = "https://lukaw3d.github.io/abi-playground-sapphire/";
-  const redirectTo = baseUrl + "?redirect=" + encodeURIComponent(window.location.href.replace(baseUrl, ""));
-  window.location.replace(redirectTo);
-  // Client-side-only code
-}
-
-export default function Custom404() {
-  return <></>;
-}
+index.tsx
