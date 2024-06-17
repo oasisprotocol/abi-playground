74cdf5fa2b4b58fc99df7475e7b1e4bce3422edd
lukaw3d <lukajeran@oasislabs.com>
2024-06-17 18:50:30 +0200

    Make footer take less width in sidebar

--- packages/nextjs/components/MiniFooter.tsx
+++ packages/nextjs/components/MiniFooter.tsx
@@ -1,25 +1,22 @@
 export const MiniFooter = () => {
   return (
-    <div className="flex justify-center items-center gap-1 text-xs w-full mt-10 z-10">
-      <div className="mb-1">
-        <span>
-          Source at:
-          <a
-            href="https://github.com/oasisprotocol/sapphire-abi-playground"
-            target="_blank"
-            rel="noreferrer"
-            className="link"
-          >
-            oasisprotocol/sapphire-abi-playground
-          </a>
-        </span>
-        &nbsp;·&nbsp;
-        <span>
-          Forked from:
-          <a href="https://github.com/BuidlGuidl/abi.ninja" target="_blank" rel="noreferrer" className="link">
-            BuidlGuidl/abi.ninja
-          </a>
-        </span>
+    <div className="text-center text-xs mt-10 mb-1 z-10">
+      <div>
+        Source at:{" "}
+        <a
+          href="https://github.com/oasisprotocol/sapphire-abi-playground"
+          target="_blank"
+          rel="noreferrer"
+          className="link"
+        >
+          oasisprotocol/sapphire-abi-playground
+        </a>
+      </div>
+      <div>
+        Forked from:{" "}
+        <a href="https://github.com/BuidlGuidl/abi.ninja" target="_blank" rel="noreferrer" className="link">
+          BuidlGuidl/abi.ninja
+        </a>
       </div>
     </div>
   );
