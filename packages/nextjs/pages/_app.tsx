40c3dc01dba85453a4223da560e9c5f2d96f736d
lukaw3d <lukajeran@oasislabs.com>
2024-07-01 17:09:23 +0200

    Add a warning banner

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -38,6 +38,26 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
     >
       <div className="flex min-h-screen flex-col">
         <main className="relative flex flex-1 flex-col">
+          <div role="alert" className="bg-error">
+            {/* Heroicons: exclamation-triangle; License: MIT */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current inline h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              />
+            </svg>
+            <span>
+              Warning: Interacting with contracts directly is risky. Be careful and use it as a last resort and with
+              small values.
+            </span>
+          </div>
           <Component {...pageProps} />
         </main>
       </div>
