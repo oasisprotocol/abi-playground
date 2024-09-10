049f47c5c63735c7de877c98f2ce9da4eb4d40e8
lukaw3d <lukajeran@oasislabs.com>
2024-09-10 05:40:27 +0200

    Replace wagmi config with default rainbowkit config
    This, or 2f7bd2b578d770a34e6e8fbabf1724f74e08dafc fixes brave wallet

--- packages/nextjs/services/web3/wagmiConfig.tsx
+++ packages/nextjs/services/web3/wagmiConfig.tsx
@@ -1,30 +1,15 @@
-import { wagmiConnectors } from "./wagmiConnectors";
-import { Chain, createClient, http } from "viem";
-import { hardhat, mainnet } from "viem/chains";
-import { createConfig } from "wagmi";
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import scaffoldConfig from "~~/scaffold.config";
-import { getAlchemyHttpUrl, getTargetNetworks } from "~~/utils/scaffold-eth";
+import { getTargetNetworks } from "~~/utils/scaffold-eth";
 
 const targetNetworks = getTargetNetworks();
 
-// We always want to have mainnet enabled (ENS resolution, ETH price, etc). But only once.
-export const enabledChains = targetNetworks.find((network: Chain) => network.id === 1)
-  ? targetNetworks
-  : ([...targetNetworks, mainnet] as const);
-
-export const wagmiConfig = createConfig({
-  chains: enabledChains as [Chain, ...Chain[]],
-  connectors: wagmiConnectors,
-  ssr: true,
-  client({ chain }) {
-    return createClient({
-      chain,
-      transport: http(getAlchemyHttpUrl(chain.id)),
-      ...(chain.id !== (hardhat as Chain).id
-        ? {
-            pollingInterval: scaffoldConfig.pollingInterval,
-          }
-        : {}),
-    });
+export const wagmiConfig = getDefaultConfig({
+  appName: "Oasis ABI Playground",
+  projectId: scaffoldConfig.walletConnectProjectId,
+  chains: targetNetworks,
+  ssr: false, // If your dApp uses server side rendering (SSR)
+  batch: {
+    multicall: false,
   },
 });
