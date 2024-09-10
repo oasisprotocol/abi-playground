1bebf60f7bfe19648619debd9163c3381668d4ba
lukaw3d <lukajeran@oasislabs.com>
2024-09-10 06:23:42 +0200

    Ignore type errors

--- packages/nextjs/services/web3/wagmiConfig.tsx
+++ packages/nextjs/services/web3/wagmiConfig.tsx
@@ -7,6 +7,7 @@ const targetNetworks = getTargetNetworks();
 export const wagmiConfig = getDefaultConfig({
   appName: "Oasis ABI Playground",
   projectId: scaffoldConfig.walletConnectProjectId,
+  // @ts-expect-error Chains type enforces non-empty array. But targetNetworks is not empty.
   chains: targetNetworks,
   ssr: false, // If your dApp uses server side rendering (SSR)
   batch: {
@@ -17,5 +18,6 @@ export const wagmiConfig = getDefaultConfig({
 wagmiConfig.connectors.forEach(c => {
   // Disable simulation - it always fails in Sapphire.
   // Upstream comment about being inverted https://github.com/wevm/wagmi/pull/3868/files#r1751172712
+  // @ts-expect-error Ignore read-only warning
   c.supportsSimulation = true;
 });
